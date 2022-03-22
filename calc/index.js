8131194281ff14645c85e0287efdc5f716da9855
Razhatawka <nataliia85sakhnova@gmail.com>
2022-03-22 18:56:21 +0200

    пока только начало, разбираюсь

--- calc/index.js
+++ calc/index.js
@@ -3,6 +3,7 @@
             let y = document.getElementById("number2").value;
          //   alert(Number(x) + Number(y));
             document.getElementById("result").innerHTML= Number(x) + Number(y)
+            console.log(document.getElementById("result").innerHTML)
         }
 
         function Minus() {
@@ -20,6 +21,42 @@
         function Divide() {
             let x = document.getElementById("number1").value;
             let y = document.getElementById("number2").value;
+            console.log(typeof y);
+            if(isNaN(y)){
+                alert("Это не число!")
+                return
+            }
+            
+            else if(!Number(y)){
+                alert("На 0 делить нельзя!") 
+                return
+            }
+
             document.getElementById("result").innerHTML= Number(x) / Number(y)
+
         }
+
+        function select(sender) {
+            console.log(sender);
+            sender.classList.toggle("selected");
+        }
+
+        function handlechange(elem) {
+            console.log(elem.value)
+            document.getElementById("color").style.background=elem.value
+        }
+
+
+        function themeselect(sender) {
+            console.log(sender);
+            sender.classList.toggle("darktheme");
+        }
+
+        function themechange(elem) {
+            console.log(elem.value)
+            document.getElementById("theme").style.background=elem.value
+        }
+
+
+
 
